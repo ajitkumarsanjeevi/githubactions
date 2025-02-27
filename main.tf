8d741dcf403429d1a46b6fca1eec15e9ccad645d
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-02-27 10:34:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Provider configuration
+provider "aws" {
+  region = "ap-sotuth-1"  
+}
+
+# Create VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main_vpc"
+  }
+}
