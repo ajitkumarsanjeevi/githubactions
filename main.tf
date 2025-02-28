5139fc1b2943b764a4b2212165e690126f879b42
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-02-28 15:52:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,14 +36,13 @@ resource "aws_security_group" "webtraffic" {
 }
 
 # Create two EC2 instances using count
-resource "aws_instance" "example" {
-  count         = length(var.instance_names)
+resource "aws_instance" "example" 
   ami           = "ami-02ddb77f8f93ca4ca"
   key_name      = "splunk"
   instance_type = "t2.micro" 
   vpc_security_group_ids = [aws_security_group.webtraffic.id]
 
   tags = {
-    Name = var.instance_names[count.index]
+    Name = instance-1
   }
 }
