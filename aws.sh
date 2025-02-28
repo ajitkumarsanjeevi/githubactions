89499a1cf72712e4e69252d6d45b548125a8e95a
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-02-28 16:13:49 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,8 +5,23 @@ ROLE_NAME="EC2-S3-FullAccess-Role"
 POLICY_ARN="arn:aws:iam::aws:policy/AmazonS3FullAccess"
 INSTANCE_ID="i-077636a4f920985df" 
 
+# Step 1: Create IAM Role
+echo "Creating IAM role with S3 full access..."
+
+aws iam create-role \
+    --role-name $ROLE_NAME \
+    --assume-role-policy-document file://trust-policy.json
+
+# Step 2: Attach S3 Full Access Policy
+echo "Attaching AmazonS3FullAccess policy to the role..."
+
+aws iam attach-role-policy \
+    --role-name $ROLE_NAME \
+    --policy-arn $POLICY_ARN
+
 # Step 3: Create a trust policy file (for EC2 service)
-TRUST-POLICY="{
+cat <<EOF > trust-policy.json
+{
   "Version": "2012-10-17",
   "Statement": [
     {
@@ -17,25 +32,8 @@ TRUST-POLICY="{
       "Action": "sts:AssumeRole"
     }
   ]
-}"
-
-
-# Step 1: Create IAM Role
-echo "Creating IAM role with S3 full access..."
-
-aws iam create-role \
-    --role-name $ROLE_NAME \
-    --assume-role-policy-document "$TRUST-POLICY"
-
-# Step 2: Attach S3 Full Access Policy
-echo "Attaching AmazonS3FullAccess policy to the role..."
-
-aws iam attach-role-policy \
-    --role-name $ROLE_NAME \
-    --policy-arn $POLICY_ARN
-
-
-
+}
+EOF
 
 # Step 4: Attach the IAM Role to the EC2 Instance
 echo "Attaching IAM role to EC2 instance $INSTANCE_ID..."
