069f29c24c0d997b3a7019a5b7ea9fd8e257712b
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-02-28 12:01:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,13 @@ variable "instance_names" {
   type        = list(string)
   default     = ["instance-1", "instance-2"]
 }
+
+variable "ingress-rules" {
+  type = list(number)
+  default = [ 22,8080,80,443 ]
+}
+
+variable "egress-rules" {
+  type = list(number)
+  default = [ 22,8080,80,443,25 ]  
+}
