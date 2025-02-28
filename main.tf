1d01dcea5e4d8b90cc325e06d64aba0ba6cda7a6
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-02-28 07:47:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,15 @@
 provider "aws" {
   region = "ap-south-1"  
 }
-
-# Create VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+# Create two EC2 instances using count
+resource "aws_instance" "example" {
+  count         = var.
+  ami           = 
+  key_name      = "splunk"
+  instance_type = "t2.micro"  # Example instance type
+  
+  # Dynamically assign names using count.index
   tags = {
-    Name = "main_vpc"
+    Name = 
   }
-}
+
