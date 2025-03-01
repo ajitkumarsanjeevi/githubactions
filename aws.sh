b41e9d09c38a7d9a197d814c869ee3042c9bd259
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-01 18:59:32 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -7,26 +7,20 @@ ROLE_NAME="Eksworkernodepolicy"
 
 
 ekswokernodepolicy()  {
-
-
-echo "Creating IAM role with S3 full access..."
-
 aws iam create-role \
     --role-name $ROLE_NAME \
     --assume-role-policy-document file://trust-policy.json
 
 # Step 2: Attach S3 Full Access Policy
-echo "Attaching AmazonS3FullAccess policy to the role..."
-
 aws iam attach-role-policy \
     --role-name $ROLE_NAME \
     --policy-arn $1
 
 }
 
-eksworkernodeplicy arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy
+eksworkernodepolicy arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy
 
 eksworkernodepolicy arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy
 
-eksworkernodepoicy arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
+eksworkernodepolicy arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
 
