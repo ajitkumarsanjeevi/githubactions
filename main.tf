ba4cc59a34b711be2ced34b3a0a507bb1a01d121
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-13 21:38:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,71 @@
-# Provider configuration 
-provider "aws" { 
-  region = "ap-south-1"  
+provider "aws" {   
+  region = "us-east-1"    
 }
 
+resource "aws_vpc" "main" {             
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true 
+  enable_dns_hostnames = true  
 
-#security group
+  tags = {
+    Name = "MainVPC"
+  }
+}
 
-resource "aws_security_group" "eks_sg" {
+resource "aws_subnet" "public_subnet_1" {  
+  vpc_id                  = aws_vpc.main.id     
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"  
+
+  tags = {
+    Name = "PublicSubnet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "PublicSubnet2"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MainIGW"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "webtraffic" {
   name        = "webtraffic"
   description = "Allow inbound and outbound traffic"
 
@@ -33,5 +92,4 @@ resource "aws_security_group" "eks_sg" {
          cidr_blocks      = ["0.0.0.0/0"] 
     }
   }
-}
-
+}	
