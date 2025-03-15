b69807d649595da1e0295947fc0d75387d68650f
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-15 11:50:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {  
+resource "aws_subnet" "public_subnet_1" {   
   vpc_id                  = aws_vpc.main.id     
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
@@ -135,20 +135,7 @@ resource "aws_iam_role" "eks_worker_node_role" {
   })
 }
 
-variable "worker_node_policies" {
-  default = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",    
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  ]
-}
 
-resource "aws_iam_role_policy_attachment" "worker_node_policies" {
-  count = length(var.worker_node_policies)
-
-  policy_arn = var.worker_node_policies[count.index]
-  role       = aws_iam_role.eks_worker_node_role.name    
-}
 
 
 
