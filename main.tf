46d93ef260e71c5dcff95970e552fcd6384f0f05
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 10:17:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,9 +107,8 @@ instance_type = each.value["instance_type]
 availability_zone = each.value["availability_zone"]
 subnet_id = aws_subnet.public_subnet_1.id
 vpc_security_group_ids = [aws_security_group.webtraffic.id]
-tags = {
-    Name = each.value["tags"]
-  }
+tags = Name = each.value["tags"]
+  
 }
 
 
