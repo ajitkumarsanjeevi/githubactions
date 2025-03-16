3ce245df97c4d26c79c7893f6d365e8da5063581
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 08:25:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,48 +94,12 @@ resource "aws_security_group" "webtraffic" {
   }
 }	
 
-# EKS Cluster IAM Role
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
+resource "aws_instance" "instance_1"
+ami = "ami-00bb6a80f01f03502"
+key_name = "splunk"
+availability_zone = "ap-south-1a"
+subnet_id = aws_subnet.public_subnet_1.id
+vpc_security_group_ids = [aws_security_group.webtraffic.id]
 }
-
-# Attach AWS Managed Policies for EKS Cluster
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role        = aws_iam_role.eks_cluster_role.name 
-}
-
-resource "aws_iam_role" "eks_worker_node_role" {        
-  name = "example-iam-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-
-
 
 
