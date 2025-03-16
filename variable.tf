75146dfea4289c7296adaa78428a761e2e1bd6f9
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 10:06:12 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -18,7 +18,8 @@ ariable "ec2_configs" {
   default = {
     instance1 = {
       instance_type = "t2.micro"
-      ami_id        = "ami-0abcdef1234567890"           
+      ami_id        = "ami-0abcdef1234567890"
+      availability_zone = "ap-south-1a"
       key_name      = "splunk"
       tags = {
         Name = "Instance1"
