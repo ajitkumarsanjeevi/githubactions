6697b9a2c386453795d205b849bd1217a7f37e21
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 14:07:33 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,16 +1,3 @@
-{                      
-
-"Version": "2012-10-17",  
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com" 
-            },
-            "Action": "sts:AssumeRole" 
-        }
-    ]
-}
 
 #!/bin/bash
 
