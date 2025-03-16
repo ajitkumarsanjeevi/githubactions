0e04cc9d5e91d64beb037868f79f4a38625b8871
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 07:57:15 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,38 +1,13 @@
-
 #!/bin/bash
 
-# Variables
+azs=$(aws ec2 describe-availability-zones --region ap-south-1 --query "AvailabilityZones[].[ZoneName]" --output text)
 
-ROLE_NAME="Eksworkernodepolicy" 
+for avaiability_zones in "$azs"
 
+do
 
-eksworkernodepolicy() {
-aws iam create-role \
-    --role-name $ROLE_NAME \
-    --assume-role-policy-document file://trust-policy.json
+echo "$avaiability_zones"
 
-# Step 2: Attach S3 Full Access Policy
-aws iam attach-role-policy \
-    --role-name $ROLE_NAME \
-    --policy-arn $1
-
-}
-
-eksworkernodepolicy "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-
-eksworkernodepolicy "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-
-eksworkernodepolicy "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-
-aws iam create-role \
-    --role-name Ekscluster \
-    --assume-role-policy-document file://masternodepolicy.json
-
-
-aws iam attach-role-policy \
-    --role-name Ekscluster \
-    --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
-
-    
+done
 
 
