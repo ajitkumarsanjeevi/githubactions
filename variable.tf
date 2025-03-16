33798cfa8506ff893f6604e5d4894ede07e379cc
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-16 09:57:06 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -12,3 +12,14 @@ variable "egress-rules" {
   type = list(number)
   default = [ 22,8080,80,443,25 ]  
 }
+
+ariable "ec2_configs" {
+  type = map(any)
+  default = {
+    instance1 = {
+      instance_type = "t2.micro"
+      ami_id        = "ami-0abcdef1234567890"           
+      key_name      = "splunk"
+      tags = {
+        Name = "Instance1"
+      }
