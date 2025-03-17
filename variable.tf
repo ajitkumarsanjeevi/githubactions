ce5f996f24ff06b57ca2b61d17df31b177104183
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-17 16:50:53 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,6 +11,6 @@ variable "ingress-rules" {
 variable "worker_node_policies" {
   description = "List of policy ARNs to attach to the EKS worker node role"
   type        = list(string)
-  default = ["arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy, "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEC2FullAccess"]
+  default = ["arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEC2FullAccess"]
 }
 
