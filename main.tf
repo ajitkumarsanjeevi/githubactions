ecf0a6eb773711e7f59753ab87fbf7a67bd3f2b3
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-17 19:40:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -120,8 +120,9 @@ resource "aws_iam_role" "eks_cluster_role" {
 
 # Attach AmazonEKSClusterPolicy to the EKS role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  count = length(var.eks_cluster_policies)
   role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = var.eks_cluster_policies[count.index]
 }
 
 
