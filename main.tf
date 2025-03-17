039252f4850b4d4831055fca6e10ff2ec23b05e9
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-17 17:36:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,6 +136,7 @@ resource "aws_eks_cluster" "eks_cluster" {
     endpoint_private_access = true
     endpoint_public_access  = true
     }
+  depends_on = [aws_subnet.public_subnet_1.id]
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
    
     
