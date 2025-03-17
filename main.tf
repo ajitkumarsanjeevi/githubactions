dd7cb85b46cd660d38314d8293a0ca517d6fb644
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-17 17:10:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 }
 
 
-resource "aws_vpc" "main" {            
+resource "aws_vpc" "main" {           
   cidr_block = "10.0.0.0/16"
   enable_dns_support   = true 
   enable_dns_hostnames = true  
