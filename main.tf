b9d6a4712f0257cdcbaa94dd0046db8551f58a07
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-17 16:48:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -170,7 +170,7 @@ tags = {
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-node-group"
-  node_role_arn   = aws_iam_role."eks_worker_role.arn 
+  node_role_arn   = aws_iam_role.eks_worker_role.arn 
   subnet_ids      = ["aws_subnet.public_subnet_1.id", "aws_subnet.public_subnet_2.id"]
   instance_types  = ["t2.medium"] 
   disk_size       = 20  
