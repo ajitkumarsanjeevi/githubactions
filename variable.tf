1395ef92f271330b36771340d86e95055c0189cb
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-20 16:49:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -18,6 +18,6 @@ variable "eks_cluster_policies" {
 variable "worker_node_policies" {
   description = "List of policy ARNs to attach to the EKS worker node role"
   type        = list(string)
-  default = ["arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEC2FullAccess", arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy]
+  default = ["arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy", "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly", "arn:aws:iam::aws:policy/AmazonEC2FullAccess", "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"]
 }
 
