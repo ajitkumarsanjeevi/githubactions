017b6c396699410d0a4db6b3c07f3dfd0694f899
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-20 15:33:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,16 +1,12 @@
-variable "instance_names" {
-  type        = list(string)
-  default     = ["instance-1"]
-}
 
 variable "ingress-rules" {
   type = list(number)
-  default = [ 22,8080,80,443,300080 ]
+  default = [ 22,8080,80,443 ]
 }
 
 variable "egress-rules" {
   type = list(number)
-  default = [ 22,8080,80,443,30080 ]
+  default = [ 22,8080,80,443 ]
 }
 
 variable "eks_cluster_policies" {
