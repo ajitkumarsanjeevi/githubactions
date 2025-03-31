b67c92efd9288b3da93b938451262a73ad447219
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-31 21:42:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terrafom-state"
-    key    = "statefile/terraform.tfstate"
+    key    = "statefile/terraform.tfstate" 
     region = "ap-south-1"
   }
 }
